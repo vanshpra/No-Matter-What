cc8ac99f108b103a3e9536632a082bb22690c316
Vanshika <prasadchumi@gmail.com>
2023-10-01 02:02:23 -0500

    Initial Commit

---
+++ No Matter What/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  No Matter What
+//
+//  Created by Venya Prasad on 10/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ No Matter What/No_Matter_WhatApp.swift
@@ -0,0 +1,17 @@
+//
+//  No_Matter_WhatApp.swift
+//  No Matter What
+//
+//  Created by Venya Prasad on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct No_Matter_WhatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
